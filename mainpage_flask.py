1d62553993cf2c4d26ac4564908e84b3ccde1259
Trevor <harmsta@student.swosu.edu>
2025-11-12 18:10:04 -0600

    Added bootstrap and template inheritance functionality.

--- mainpage_flask.py
+++ mainpage_flask.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "<h1>Welcome to OpenGate.fm!</h1>"
-
 @app.route('/<name>')
-def user(name):
-    return f"<h2>Hello, {name}! This is your user page.</h2>"
+def home(name):
+    return render_template('index.html', genres = ["Rock", "Pop", "Rap", "Jazz", "Blues"], name=name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
